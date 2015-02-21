3fe84897d1181d809f3e0cd03c264a6adccf47c0
RaulJimenezOrtega <hhkaos@gmail.com>
2015-02-21 13:34:23 +0100

    Updating translations

--- lavega/string.php
+++ lavega/string.php
@@ -32,112 +32,109 @@
 						   ";
 	
 	$menu_detail["es"] = "Detalles";
-	$menu_detail["fr"] = "";
+	$menu_detail["fr"] = "Détails";
 	$menu_detail["en"] = "Details";
-					 
+
 	$menu_reviews["es"] = "Opiniones";
-	$menu_reviews["fr"] = "";
+	$menu_reviews["fr"] = "Appréciations";
 	$menu_reviews["en"] = "Reviews";
-					 
+
 	$menu_avail["es"] = "Disponibilidad";
-	$menu_avail["fr"] = "";
+	$menu_avail["fr"] = "Disponibilité";
 	$menu_avail["en"] = "Availability";
-					 
+
 	$menu_contact["es"] = "Contacto";
-	$menu_contact["fr"] = "";
+	$menu_contact["fr"] = "Contact";
 	$menu_contact["en"] = "Contact";
-					 
+
 	$menu_loc["es"] = "Localizacion";
-	$menu_loc["fr"] = "";
+	$menu_loc["fr"] = "Localisation";
 	$menu_loc["en"] = "Location";
 
 	$bedrooms["es"] = "Dormitorios:";
-	$bedrooms["fr"] = ":";
+	$bedrooms["fr"] = "Dormitories:";
 	$bedrooms["en"] = "Bedrooms:";
 
 	$bedrooms_det["es"] = "1 Dormitorio con cama de matrimonio.<br>2 Dormitorios con dos camas individuales cada uno.";
-	$bedrooms_det["fr"] = "";
+	$bedrooms_det["fr"] = "1 Chambre avec double , 150x190 .<br>2 chambres avec deux lits simples 90x190 chacune.";
 	$bedrooms_det["en"] = "Bedroom 1 with double bed.<br>Bedroom 2 and 3 with twin beds.";
 
 
 	$bathroom["es"] = "Baños";
-	$bathroom["fr"] = "";
+	$bathroom["fr"] = "Salles de bain";
 	$bathroom["en"] = "Bathrooms";
 
-	$bathroom_det["es"] =   "Cuarto de baño completo dentro del dormitorio principal. <br>
-							Otro cuarto de baño completo en el pasillo.<br>
-							Un aseo con váter y lavabo en la planta baja.";
-	$bathroom_det["fr"] =   "";
+	$bathroom_det["es"] =   "Cuarto de baño 1 en el dormitorio principal con bañera, cuarto de baño 2 con bañera, inodoro, bidet. <br>
+							Otro cuarto de baño con bañera, Cuarto de baño con bañera, inodoro, bidet. <br>
+							Baño separado con lavabo en descansillo de la entrada.";
+	$bathroom_det["fr"] =   "2 Salles de bain avec baignoire, bidet, lavabo, wc., 1 Toilette séparée avec lavabo et wc.";
 	$bathroom_det["en"] =   "En-suite bathroom to bedroom 1 with bath, wc Bathroom with bath over, wc, bidet.<br>
 							Other bathroom with bath, wc Bathroom with bath over, wc, bidet.<br>
 							Separate wc with sink in entrance hall.";
 
 	$kitchen["es"] = "Cocina:";
-	$kitchen["fr"] = "";
+	$kitchen["fr"] = "Cuisine";
 	$kitchen["en"] = "Kitchen:";
 
-	$kitchen_det["es"] = "Totalmente equipada, con horno, lavavajillas, microondas, lavadora, tostadora, etc.";	
-	$kitchen_det["fr"] = "";	
-	$kitchen_det["en"] = "Fully fitted, including full oven, dishwasher, microwave, washing machine, toaster etc.";	
+	$kitchen_det["es"] = "Totalmente equipada, con horno, microondas, lavavajillas, lavadora, tostadora, etc.";
+	$kitchen_det["fr"] = "Entièrement équipée avec four, micro-ondes, lave-vaisselle, machine à laver , grille-pain, etc.";
+	$kitchen_det["en"] = "Fully fitted, including full oven, microwave, dishwasher, washing machine, toaster etc.";
 
 	$liv_room["es"] = "Salón:";
-	$liv_room["fr"] = "";
+	$liv_room["fr"] = "Salon et salle à manger";
 	$liv_room["en"] = "Living Rooms:";
 
 	$liv_room_det["es"] = "Salón-comedor con TV, TV vía satélite con canales en inglés, francés, alemán, italiano ... (satélites Astra 28,2º y Astra 19 °) DVD, etc.";
-	$liv_room_det["fr"] = "";
+	$liv_room_det["fr"] = "Salon avec TV, chaînes satellite en anglais, français, allemand, italien ... (satellites Astra Astra 28,2 ° et 19 °) DVD, etc.";
 	$liv_room_det["en"] = "Dining room whit equipment TV, Satellite tv with English channels, French, German, Italian...(satellites: astra 28.2 ° and astra 19 °) DVD, etc.";
 
 	$other["es"] = "Otro:";
-	$other["fr"] = "";
+	$other["fr"] = "D'autres";
 	$other["en"] = "Other:";
 
 	$other_det["es"] = "Hay unas escaleras internas que llegan a una azotea privada para tomar el sol terraza con fantásticas vistas al mar y las montañas.";
-	$other_det["fr"] = "";
+	$other_det["fr"] = "Avec solarium, 2ème étage, les bains de soleil, avec de magnifiques vue mer et montagne.";
 	$other_det["en"] = "Internal stairs lead to a roof top sunbathing terrace with fantastic views of the sea and mountains.";
 
 	$cleaning["es"] = "Servicio de limpieza / toallas / ropa:";
-	$cleaning["fr"] = "";
+	$cleaning["fr"] = "Service de ménage. Serviettes et draps";
 	$cleaning["en"] = "Cleaning / Towels / Linen / Maid service:";
 
 	$cleaning_det["es"] = "La casa se limpia para la llegada, las toallas y sábanas se cambian semanalmente. Por favor traiga propias toallas de playa para uso de la piscina y la playa.";
-	$cleaning_det["fr"] = "";
+	$cleaning_det["fr"] = "La maison est nettoyée avant l'arrivée, les serviettes et les draps sont changés chaque semaine. S'il vous plaît, apporter des serviettes de plage propres pour l'utilisation de la piscine et de la plage";
 	$cleaning_det["en"] = "The villa is cleaned for arrival, towels and linen are changed weekly. Please bring own beach towels for use by the pool and the beach.";
 
 	$facilities["es"] = "Servicios / Instalaciones";
-	$facilities["fr"] = "";
+	$facilities["fr"] = "Services / Équipement";
 	$facilities["en"] = "Amenities / Facilities:";
 
-	$facilities_det["es"] = "Barbacoa, Piscina privada, Parking. <br>
-							Lavavajillas, Nevera / Congelador, Cocina / estufa, plancha, microondas, horno, lavadora. <br>
-							Cuna, vía satélite, TV.";
-	$facilities_det["fr"] = "";
-	$facilities_det["en"] = "Barbecue, Private Pool, Parking. <br>
-							Dishwasher, Fridge/Freezer, Hob/Stove, Iron, Microwave, Oven, Washing Machine. <br>
-							Cot, Satellite, TV.";
+	$facilities_det["es"] = "Barbacoa a gas, Piscina privada, Parking. <br>	Mesas y sillas de terrazas, tumbonas, parasol.<br>
+							Posibilidad de Cuna.";
+	$facilities_det["fr"] = "Barbecue à gaz, Piscine privée, Parking. .<br>Tables et chaises de terrasses, chaises longues, parasol.<br> Possibilité Lit bébé.";
+	$facilities_det["en"] = "Barbecue, Private Pool, Parking. <br> Tables and chairs terraces, loungers, parasol. <br> Possibility of a cot.";
 
 	$check_others["es"] = "Otras propiedades similares:";
-	$check_others["fr"] = "";
+	$check_others["fr"] = "Propriétés similaires:";
 	$check_others["en"] = "Check other properties:";
 
-	$more_reviews["es"] = 'Puedes leer más opiniones en <a href="http://www.homeaway.es/p484671?refid=property_link&catid=484671#reviews">homeaway.es</a> y en <a href="http://www.ownersdirect.co.uk/accommodation/p8045197#reviews">ownersdirect.co.uk</a>';
-	$more_reviews["fr"] = "";
-	$more_reviews["en"] = 'You can read more reviews in <a href="http://www.ownersdirect.co.uk/accommodation/p8045197#reviews">ownersdirect.co.uk</a> and <a href="http://www.homeaway.es/p484671?refid=property_link&catid=484671#reviews">homeaway.es</a>';
-	
+	//$more_reviews["es"] = 'Puedes leer más opiniones en <a href="http://www.homeaway.es/p484671?refid=property_link&catid=484671#reviews">homeaway.es</a> y en <a href="http://www.ownersdirect.co.uk/accommodation/p8045197#reviews">ownersdirect.co.uk</a>';
+	//$more_reviews["fr"] = 'Vous pouvez lire plus de commentaires dans <a href="http://www.homeaway.es/p484671?refid=property_link&catid=484671#reviews">homeaway.es</a> et dans <a href="http://www.ownersdirect.co.uk/accommodation/p8045197#reviews">ownersdirect.co.uk</a>';
+	//$more_reviews["en"] = 'You can read more reviews in <a href="http://www.ownersdirect.co.uk/accommodation/p8045197#reviews">ownersdirect.co.uk</a> and <a href="http://www.homeaway.es/p484671?refid=property_link&catid=484671#reviews">homeaway.es</a>';
+
 	$availability_txt1["es"] = "En cada semana aparece el precio del alojamiento.<br>
-					    		Si tienen alguna duda, por favor, contáctame bien por e-mail o por teléfono: +34-655087577.";
-    $availability_txt1["fr"] = "";
+					    		Si tienen alguna duda, por favor, contáctame bien por e-mail nerja@gmx.es o por teléfono: +34-655087577.";
+    $availability_txt1["fr"] = "Chaque semaine, le prix du logement apparaît <br> Si vous avez des questions, s'il vous plaît contactez-moi par e-mail: nerja@gmx.es ou par téléphone. + 34-655087577";
 	$availability_txt1["en"] = "The rental price appears every week. <br>
-								If you have any questions, please contact me by e-mail or by phone: + 34-655087577.";
+								If you have any questions, please contact me by e-mail nerja@gmx.es or by phone: + 34-655087577.";
 
 
 	$availability_txt2["es"] = "HORARIO DE ENTRADA Y SALIDA";
-	$availability_txt2["fr"] = "";
+	$availability_txt2["fr"] = "HEURES";
 	$availability_txt2["en"] = "TIME OF ENTRY AND EXIT";
 
 	$availability_txt3["es"] = "Horario de entrada: El primer día del alquiler a partir de las 15.00 horas (en algunos casos es posible adelantarlo, según disponibilidad).Horario de salida: Al día siguiente del último día de alquiler antes de las 9.30 horas de la mañana.";
-	$availability_txt3["fr"] = "";
-	$availability_txt3["en"] = "Check In Time: The first rent day from 15.00 (in some cases it is possible to advance, subject to availability). Departure time: The day after the last day of rental before 9:30 am.";
+	$availability_txt3["fr"] = "Arrivée: Le premier jour de location à partir de 15,00 (dans certains cas, il est possible d'avancer, sous réserve de disponibilité). Départ: le dernier jour de location avant 09h30.";
+	$availability_txt3["en"] = "Check In: The first rent day from 15.00 (in some cases it is possible to advance, subject to availability). Check Out: the last day of rental before 9:30 am.";
 
 	$Enero["es"] = "Enero";
 	$Enero["fr"] = "";
--- string.php
+++ string.php
@@ -1,18 +1,18 @@
 <?php
 
 	$header_h1["es"] = "Vacaciones en Nerja, Costa del Sol";
-	$header_h1["fr"] = "";
+	$header_h1["fr"] = "Vacances à Nerja, Costa del Sol";
 	$header_h1["en"] = "Holidays in Nerja, Costa del Sol";
 
 	$claim["es"] = "Ayuda para disfrutar tus vacaciones en Nerja";
-	$claim["fr"] = "";
+	$claim["fr"] = "Aide pour profiter de vos vacances à Nerja";
 	$claim["en"] = "Help to enjoy your holidays Nerja";
 
 	$btn["es"] = "Ver guía interactiva";
-	$btn["fr"] = "";
+	$btn["fr"] = "Voir guide interactif";
 	$btn["en"] = "View interactive guide";
 
 	$properties["es"] = "Alojamiento:";
-	$properties["fr"] = "";
-	$properties["en"] = "Our properties:";
+	$properties["fr"] = "Logements:";
+	$properties["en"] = "Our properties:"; 
 ?>
